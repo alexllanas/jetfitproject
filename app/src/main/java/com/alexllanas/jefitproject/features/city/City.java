1e34285c16cf25d7de041d49df358021d6182926
alexllanas <llana004@rangers.uwp.edu>
2022-06-08 15:12:13 -0500

    Add: City recyclerview adapter

--- app/src/main/java/com/alexllanas/jefitproject/features/city/City.java
+++ app/src/main/java/com/alexllanas/jefitproject/features/city/City.java
@@ -1,4 +1,27 @@
 package com.alexllanas.jefitproject.features.city;
 
+import com.alexllanas.jefitproject.features.business.Business;
+
+import java.util.ArrayList;
+
 public class City {
+    public String name;
+    public ArrayList<Business> businessList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Business> getBusinessList() {
+        return businessList;
+    }
+
+    public void setBusinessList(ArrayList<Business> businessList) {
+        this.businessList = businessList;
+    }
+
 }
--- app/src/main/java/com/alexllanas/jefitproject/features/city/CityAdapter.java
+++ app/src/main/java/com/alexllanas/jefitproject/features/city/CityAdapter.java
@@ -1,4 +1,49 @@
 package com.alexllanas.jefitproject.features.city;
 
-public class CityAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.alexllanas.jefitproject.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CityAdapter extends RecyclerView.Adapter<CityAdapter.CityViewHolder> {
+
+    private ArrayList<City> cities = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public CityAdapter.CityViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_city, parent, false);
+        return new CityViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CityAdapter.CityViewHolder holder, int position) {
+        String cityName = cities.get(position).getName();
+        holder.cityNameTextView.setText(cityName);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cities.size();
+    }
+
+    public class CityViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView cityNameTextView;
+
+        public CityViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            cityNameTextView = (TextView) itemView.findViewById(R.id.city_name);
+        }
+
+    }
 }
