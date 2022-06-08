54bc602de8f0ffb4bff1e4af38ba1c931110fd49
alexllanas <llana004@rangers.uwp.edu>
2022-06-08 14:04:39 -0500

    Add: business detail layout

--- app/src/main/java/com/alexllanas/jefitproject/ui/MainApplication.java
+++ app/src/main/java/com/alexllanas/jefitproject/ui/MainApplication.java
@@ -1,4 +1,24 @@
 package com.alexllanas.jefitproject.ui;
 
-public class MainApplication {
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.core.ImageTranscoderType;
+import com.facebook.imagepipeline.core.MemoryChunkType;
+
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(
+                this,
+                ImagePipelineConfig.newBuilder(this)
+                        .setMemoryChunkType(MemoryChunkType.BUFFER_MEMORY)
+                        .setImageTranscoderType(ImageTranscoderType.JAVA_TRANSCODER)
+                        .experiment().setNativeCodeDisabled(true)
+                        .build());
+
+    }
 }
